766ea33d2a5af78d2545b37f679b19051bfc6caf
ALLUK <lam83@bk.ru>
2022-04-15 19:39:11 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GetWorkWithObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myOne = new();
+            MyClass myTwo = new();
+
+            myOne.x = 1;
+            myTwo.x = 2;
+
+            Console.WriteLine(myOne.x);
+
+            myOne = myTwo;
+            myTwo.x = 12;
+
+            Console.WriteLine(myOne.x);
+            Console.WriteLine(myTwo.x);
+        }
+    }
+    class MyClass
+    {
+        public int x;
+    }
+}
