30571ba1378d8f60150fc2cb1c45313ef202625d
ALLUK <lam83@bk.ru>
2022-04-16 23:56:05 +0500

    Решение первой задачик третьему уроку

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -7,30 +6,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace GetWorkWithObject
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string str = "12";
-            int number = 123;
-
-            DoWork(str);
-            DoAnotherWork(number);
-
-            Console.WriteLine(str);
-            Console.WriteLine(number);
+            PaymentBanks paymentBanks = new();
+            //paymentBanks.GetNumberAccount();
+            //paymentBanks.GetBalanceAccount();
+            //paymentBanks.GetTypeAccount();
+            paymentBanks.GetTransferMoney();
         }
-
-        static void DoWork(string referenceStr)
+    }        
+    class PaymentBanks
+    {
+        int _FirstNumberAccount { get; set; }
+        int _SecondNumberAccount { get; set; }
+        int _BalanceAccount { get; set; }
+        string _TypeAccount { get; set; }
+        int _SumTransferMoney { get; set; }
+        /// <summary>
+        /// получение номера банковского счета
+        /// </summary>
+        /// <param name="numberAccount"></param>
+        public void GetNumbersAccount(int firstNumberAccount)
         {
-            referenceStr = "12345";
+            _ = _FirstNumberAccount;
+            
         }
-        static void DoAnotherWork(int valueInteger)
+        /// <summary>
+        /// получение баланса счета
+        /// </summary>
+        /// <param name="balanceAccount"></param>
+        public void GetBalanceAccount(int balanceAccount)
         {
-            valueInteger = 1234;
+            _ = _BalanceAccount;
+        }
+        /// <summary>
+        /// получение типа банковского счета
+        /// </summary>
+        /// <param name="typeAccount"></param>
+        public void GetTypeAccount(string typeAccount)
+        {
+            _ = _TypeAccount;
+        }
+        /// <summary>
+        /// метод, который переводит деньги с одного счета на другой
+        /// </summary>
+        /// <param name="transferMoney"></param>
+        public void GetTransferMoney()
+        {
+            int sumTransferMoney = _SumTransferMoney;
+            int firstNumberAccount = _FirstNumberAccount;
+            //int secondNumberAccount = _SecondNumberAccount;
+            firstNumberAccount = 8696;
+            PaymentBanks paymentBanks = new();
+            paymentBanks.GetNumbersAccount(firstNumberAccount);
+
+            
+            sumTransferMoney = 3000000;
+            Console.WriteLine("На счет {0} поступило {1} рублей",firstNumberAccount,sumTransferMoney);
+
         }
     }
 }
+/*1. В класс банковский счет, созданный в упражнениях, добавить метод, который переводит деньги с одного счета на другой. 
+ * У метода два параметра: ссылка на объект класса банковский счет откуда снимаются деньги, второй параметр – сумма*/
